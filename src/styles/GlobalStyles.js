148040f36aeb5ed28ded879b8427a54d939c7dfe
jihyeon57 <ehwlgus22@gmail.com>
2022-03-23 04:48:06 +0900

    💄 Design: 전역 스타일링

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,15 +4,30 @@ import reset from 'styled-reset';
 const GlobalStyles = createGlobalStyle`
 	//style reset
 	${reset}
+	* {
+		box-sizing:border-box;
+	}
 	body {
-		
+		letter-spacing: -2px;
+		background-color: #fafafa;
   }
 	a {
 		text-decoration: none;
+		color:#1f334a;
+	}
+	p {
+		color:#1f334a;
 	}
 	ul {
 		margin:0;
 		padding:0;
+	}
+	.readable-hidden {
+		position: absolute;
+		width: 1px;
+		height: 1px;
+		overflow: hidden;
+		clip: rect(0,0,0,0);
 	}
 
 	:root {
